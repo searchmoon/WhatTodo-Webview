7aa65c7447bf7fb697dafa08bb279eec8fa08205
jeongeun moon <moon901212@gmail.com>
2025-04-29 23:02:55 +0900

    longpress wrapper 추가

---
+++ src/components/common/LongPressWrapper.tsx
@@ -0,0 +1,18 @@
+import { ReactNode } from "react";
+import { useLongPress } from "@/hooks/useLongPress";
+
+interface LongPressWrapperProps {
+  onLongPress: () => void;
+  onClick: () => void;
+  children: ReactNode;
+}
+
+export default function LongPressWrapper({
+  onLongPress,
+  onClick,
+  children,
+}: LongPressWrapperProps) {
+  const longPressEvent = useLongPress({ onLongPress, onClick, delay: 500 });
+
+  return <div {...longPressEvent}>{children}</div>;
+}
