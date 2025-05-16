4a674b7d577bc78eb283cb56e9dbef560e8391ef
jeongeun moon <moon901212@gmail.com>
2025-05-16 17:49:25 +0900

    bottomSheets content 디자인 수정

--- src/components/contents/bottomSheets/TodoContent.tsx
+++ src/components/contents/bottomSheets/TodoContent.tsx
@@ -1,7 +1,6 @@
 import { useState, useEffect } from "react";
 import { ArrowUp, CalendarIcon } from "lucide-react";
 import { cn } from "@/lib/utils";
-import InputLine from "@/components/common/InputLine";
 import { Label } from "@/components/ui/label";
 import { Calendar } from "@/components/ui/calendar";
 import {
@@ -12,6 +11,7 @@ import {
 import { Switch } from "@/components/ui/switch";
 import { TodoState, useTodoStore } from "@/store/useTodoStore";
 import dayjs from "dayjs";
+import { Input } from "@/components/ui/input";
 
 interface TodoContentProps {
   mode?: "create" | "update";
@@ -39,7 +39,7 @@ const TodoContent = ({ mode = "create", onClose }: TodoContentProps) => {
 
   const handleSubmit = () => {
     if (!todo.todo.trim()) {
-      return alert("할일을 작성해 주세요.");
+      return alert("할 일을 작성해 주세요.");
     }
 
     let updatedList: TodoState[] = [];
@@ -66,48 +66,54 @@ const TodoContent = ({ mode = "create", onClose }: TodoContentProps) => {
   };
 
   return (
-    <div className="flex flex-col items-center gap-4 mb-10">
-      <div className="flex items-center gap-4">
-        <InputLine
-          onChange={handleChange}
-          id="todo"
-          value={todo.todo}
-          placeholder="할 일을 작성해 주세요."
-        />
-      </div>
-      <div className="flex items-center gap-4">
-        <Label htmlFor="due_date" className="text-right">
-          날짜
-        </Label>
-        <Popover modal>
-          <PopoverTrigger asChild>
-            <button
-              className={cn(
-                "flex items-center w-[280px] justify-start text-left font-normal border rounded-md p-2",
-                !todo.date && "text-muted-foreground"
-              )}
-            >
-              <CalendarIcon className="mr-2 h-4 w-4" />
-              {todo.date ? (
-                dayjs(todo.date).format("YYYY-MM-DD")
-              ) : (
-                <p>날짜를 선택하세요</p>
-              )}
-            </button>
-          </PopoverTrigger>
-          <PopoverContent className="w-auto p-0">
-            <Calendar
-              mode="single"
-              selected={todo.date}
-              onSelect={(selectedDate) => {
-                if (selectedDate) {
-                  setTodo((prev) => ({ ...prev, date: selectedDate }));
-                }
-              }}
-              initialFocus
-            />
-          </PopoverContent>
-        </Popover>
+    <div className="flex flex-col items-center gap-4 mb-10 px-4">
+      <div className="flex w-full gap-2 items-center">
+        <div className="flex flex-col gap-3 w-full">
+          <Input
+            value={todo.todo}
+            type="text"
+            placeholder="할 일을 작성해 주세요."
+            onChange={handleChange}
+            className="w-full"
+          />
+
+          <Popover modal>
+            <PopoverTrigger asChild>
+              <button
+                className={cn(
+                  "flex items-center w-full justify-start text-left font-normal border rounded-md p-2",
+                  !todo.date && "text-muted-foreground"
+                )}
+              >
+                <CalendarIcon className="mr-2 h-4 w-4" />
+                {todo.date ? (
+                  dayjs(todo.date).format("YYYY-MM-DD")
+                ) : (
+                  <p>날짜를 선택하세요</p>
+                )}
+              </button>
+            </PopoverTrigger>
+            <PopoverContent className="w-auto p-0">
+              <Calendar
+                mode="single"
+                selected={todo.date}
+                onSelect={(selectedDate) => {
+                  if (selectedDate) {
+                    setTodo((prev) => ({ ...prev, date: selectedDate }));
+                  }
+                }}
+                initialFocus
+              />
+            </PopoverContent>
+          </Popover>
+        </div>
+
+        <button
+          onClick={handleSubmit}
+          className="bg-gray-400 p-2 rounded-full cursor-pointer h-fit"
+        >
+          {mode === "create" ? <ArrowUp color="#fff" /> : "수정"}
+        </button>
       </div>
 
       <div className="flex items-center gap-4">
@@ -120,13 +126,6 @@ const TodoContent = ({ mode = "create", onClose }: TodoContentProps) => {
           onCheckedChange={handleChangeSwitch}
         />
       </div>
-
-      <button
-        onClick={handleSubmit}
-        className="bg-gray-400 p-2 rounded-full cursor-pointer"
-      >
-        {mode === "create" ? <ArrowUp color="#fff" /> : "수정"}
-      </button>
     </div>
   );
 };
