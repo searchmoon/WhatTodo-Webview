d6ed204f00f9a7daed334198168ba2ac26a6678a
jeongeun moon <moon901212@gmail.com>
2025-05-17 00:46:04 +0900

    bottomSheets content 디자인 수정

--- src/components/contents/bottomSheets/TodoContent.tsx
+++ src/components/contents/bottomSheets/TodoContent.tsx
@@ -67,64 +67,66 @@ const TodoContent = ({ mode = "create", onClose }: TodoContentProps) => {
 
   return (
     <div className="flex flex-col items-center gap-4 mb-10 px-4">
-      <div className="flex w-full gap-2 items-center">
-        <div className="flex flex-col gap-3 w-full">
-          <Input
-            value={todo.todo}
-            type="text"
-            placeholder="할 일을 작성해 주세요."
-            onChange={handleChange}
-            className="w-full"
-          />
-
-          <Popover modal>
-            <PopoverTrigger asChild>
-              <button
-                className={cn(
-                  "flex items-center w-full justify-start text-left font-normal border rounded-md p-2",
-                  !todo.date && "text-muted-foreground"
-                )}
-              >
-                <CalendarIcon className="mr-2 h-4 w-4" />
-                {todo.date ? (
-                  dayjs(todo.date).format("YYYY-MM-DD")
-                ) : (
-                  <p>날짜를 선택하세요</p>
-                )}
-              </button>
-            </PopoverTrigger>
-            <PopoverContent className="w-auto p-0">
-              <Calendar
-                mode="single"
-                selected={todo.date}
-                onSelect={(selectedDate) => {
-                  if (selectedDate) {
-                    setTodo((prev) => ({ ...prev, date: selectedDate }));
-                  }
-                }}
-                initialFocus
-              />
-            </PopoverContent>
-          </Popover>
+      <div className="flex flex-col gap-3 w-full ">
+        <Popover modal>
+          <PopoverTrigger asChild>
+            <button
+              className={cn(
+                "flex items-center w-full justify-center text-left font-normal pl-2",
+                !todo.date && "text-muted-foreground"
+              )}
+            >
+              <CalendarIcon className="mr-2 h-4 w-4" />
+              {todo.date ? (
+                dayjs(todo.date).format("MM-DD (dd)")
+              ) : (
+                <p>날짜를 선택하세요</p>
+              )}
+            </button>
+          </PopoverTrigger>
+          <PopoverContent className="w-auto p-0">
+            <Calendar
+              mode="single"
+              selected={todo.date}
+              onSelect={(selectedDate) => {
+                if (selectedDate) {
+                  setTodo((prev) => ({ ...prev, date: selectedDate }));
+                }
+              }}
+              initialFocus
+            />
+          </PopoverContent>
+        </Popover>
+        <div className="space-y-2">
+          <div className="relative">
+            <Input
+              id="todo"
+              value={todo.todo}
+              type="text"
+              placeholder="할 일을 작성해 주세요."
+              onChange={handleChange}
+              className="pr-12 border-gray-200 focus:border-gray-400 focus:ring-gray-400 rounded-xl h-12 text-base"
+              noneFocus={true}
+            />
+            <button
+              onClick={handleSubmit}
+              className="absolute right-2 top-1/2 -translate-y-1/2 bg-gray-400 hover:bg-gray-600 transition-colors p-1 rounded-full cursor-pointer h-fit"
+              disabled={!todo.todo.trim() || !todo.date}
+            >
+              {mode === "create" ? <ArrowUp size="20" color="#fff" /> : "수정"}
+            </button>
+          </div>
         </div>
-
-        <button
-          onClick={handleSubmit}
-          className="bg-gray-400 p-2 rounded-full cursor-pointer h-fit"
-        >
-          {mode === "create" ? <ArrowUp color="#fff" /> : "수정"}
-        </button>
-      </div>
-
-      <div className="flex items-center gap-4">
-        <Label htmlFor="carry_over" className="text-right">
-          다음날로 일정 넘기기
-        </Label>
-        <Switch
-          id="carry_over"
-          checked={todo.carryOver}
-          onCheckedChange={handleChangeSwitch}
-        />
+        <div className="flex items-center justify-center gap-4">
+          <Label htmlFor="carry_over" className="text-right">
+            다음날로 일정 넘기기
+          </Label>
+          <Switch
+            id="carry_over"
+            checked={todo.carryOver}
+            onCheckedChange={handleChangeSwitch}
+          />
+        </div>
       </div>
     </div>
   );
